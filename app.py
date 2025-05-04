a5a8e05444210a7bdc0d087aa4a696772006bafe
William Bruno <willbc.silva@gmail.com>
2025-05-04 08:17:57 -0300

    return

--- app.py
+++ app.py
@@ -797,4 +797,4 @@ def steam_callback():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
